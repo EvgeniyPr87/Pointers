e3311938fd9ad3bfcc8ba8465ac4e2e19937c1f4
ypron <ypron@LAPTOP-5H1KDVCM>
2025-04-12 13:00:24 +0300

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void main() {
+	setlocale(LC_ALL, "");
+	cout << "Hello Pointer" << endl;
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;//вывод переменной на экран
+	cout << &a << endl;// взятие адреса переменной при выводе
+	cout << pa << endl;
+	cout << *pa << endl;
+}
---
+++ TransferParametres/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Exchenge(int a, int b);
+void main() {
+
+	setlocale(LC_ALL, "");
+
+	int a = 2, b = 3;
+	cout << a<<"\t"<< b << endl;
+	Exchenge(a, b);
+	cout << a << "\t" << b << endl;
+}
+void Exchenge(int a, int b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+}
